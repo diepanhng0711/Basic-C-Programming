45852519faf68a0eff7a967883bf0f71be3f738e
diepanhng_0711 <tue.nt194710@sis.hust.edu.vn>
2022-05-24 16:03:25 +0700

    Update the exercise of HanoiTower thaphanoi.c

--- Recursion/thaphanoi.c
+++ Recursion/thaphanoi.c
@@ -2,22 +2,26 @@
 #include<math.h>
 #include<string.h>
 
-void towers(int n, char cot1, char cot3, char cot2){
+void swap(int n, char a, char b) {
+    printf("Chuyen dia thu %d tu cot %c sang cot %c!\n",n, a, b); 
+}
+
+void towers(int n, char cot1, char cot2, char cot3){
     if (n == 1) {
-        printf("Chuyen 1 dia tu cot 1 sang cot 3!\n");
+        swap(n, cot1, cot3);
         return;
     }
-    towers(n - 1,cot1, cot2, cot3);
-    printf("Chuyen %d dia tu cot 1 sang cot 3!\n",n-1);
-    towers(n -1, cot3, cot2, cot1);
+    towers(n - 1,cot1, cot3, cot2);         //cot3 trung gian. Chuyen n-1 dia tu coc dau sang coc trung gian        A->B
+    swap(n, cot1, cot3);                    //Chuyen dia thu n tu coc dau sang coc dich                             A->C
+    towers(n -1, cot2, cot1, cot3);         //Chuyen n-1 dia tu coc trung gian sang coc dich (xuat hien de quy)     B->C
 }
 
 int main()
 {
     int num;
-    printf("Mhap so dia : ");   
+    printf("Nhap so dia : ");   
     scanf("%d", &num);
-    printf("The sequence of moves involved in the Tower of Hanoi are :\n");
-    towers(num, 'A', 'C', 'B');
+    printf("Ket qua :\n\n");
+    towers(num, 'A', 'B', 'C');
     return 0;
 }
